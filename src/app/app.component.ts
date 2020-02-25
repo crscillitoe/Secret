0d453eacac0fde044e202ec7bf70b418206136ff
crscillitoe <crscillitoe@wpi.edu>
2020-02-24 17:13:35 -0800

    first build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'Secret';
+export class AppComponent implements OnInit {
+  messages: Message[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get("https://yenlee-4a86e.firebaseio.com/message.json")
+      .subscribe((data: any) => {
+        for (let key of Object.keys(data)) {
+          console.log(key);
+          this.messages.push({
+            header: data[key]["header"],
+            message: data[key]["message"]
+          });
+        }
+      });
+  }
+}
+
+interface Message {
+  header: string;
+  message: string;
 }
